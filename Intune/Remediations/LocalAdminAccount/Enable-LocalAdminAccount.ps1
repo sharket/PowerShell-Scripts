bf96a0f362a22991d7184453aa727b0cf053dc59
sharket <luk.online@pm.me>
2023-08-02 18:50:41 +0100

    Added ErrorAction Stop, so Catch can... catch it

--- Intune/Remediations/LocalAdminAccount/Enable-LocalAdminAccount.ps1
+++ Intune/Remediations/LocalAdminAccount/Enable-LocalAdminAccount.ps1
@@ -3,16 +3,16 @@
     Enable built-in local admin account.
 
     .NOTES
-    Version:                1.0
+    Version:                1.1
     Author:                 https://github.com/sharket
     Created:                28/07/2023
-    Last Updated:           N/A
+    Last Updated:           02/08/2023
 #>
 
 $localAdmin = Get-CimInstance -ClassName Win32_UserAccount -Filter "LocalAccount = TRUE and SID like 'S-1-5-%-500'"
 
 Try {
-    Enable-LocalUser $localAdmin.Name
+    Enable-LocalUser $localAdmin.Name -ErrorAction Stop
     Write-Output "SUCCESS: Local admin account $($localAdmin.Name) enabled."
 }
 Catch {
